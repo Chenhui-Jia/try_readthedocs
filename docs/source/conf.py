e93aec7c7c324eec8fb0f2fc3cdf20c29f00dd29
Chenhui-Jia <jiachenhui@126.com>
2021-06-25 15:44:09 +0800

    update again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../aligned-hierarchies/'))
+sys.path.insert(0, os.path.abspath('../../aligned-hierarchies/'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -35,7 +35,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',]
+    'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. ah documentation master file, created by
+   sphinx-quickstart on Fri Jun 25 15:26:29 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ah's documentation!
+==============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
